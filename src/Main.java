70efdf74d61d54153e63eff379f9059dfdef0fd0
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-12-08 16:55:38 +0300

    fix to logic in main

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Main {
+
+    public static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+            "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
+            "ut aliquip ex ea commodo consequat. Duis aute irure LLL dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
+            "deserunt mollit anim id est laborum. LL ZZZ";
+
+    public static void main(String[] args) {
+
+        Map<Character, Integer> textSearch = new HashMap<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            char tmpKey = text.charAt(i);
+
+            if (Character.isLetter(tmpKey) && textSearch.get(tmpKey) == null) {
+                int value = 1;
+                textSearch.put(tmpKey, value);
+            } else if (Character.isLetter(tmpKey) && textSearch.containsKey(tmpKey)) {
+                int newValue = textSearch.remove(tmpKey) +1;
+                textSearch.put(tmpKey, newValue);
+            }
+
+
+//            if (Character.isLetter(tmp)) {
+//                int key = 1;
+//                textSearch.put(tmp, key);
+//
+//                if (textSearch.containsKey(tmp)) {
+//                    int tmpkey = textSearch.get(tmp);
+//                    textSearch.put(tmp, tmpkey + 1);
+//                }
+        }
+        System.out.println(textSearch);
+
+
+    }
+}
