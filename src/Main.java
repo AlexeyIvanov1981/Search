2c868d3834c998586e5c8b7a98471482da467c75
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2022-12-08 13:37:47 +0300

    add logic

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
             "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
             "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
             "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
-            "deserunt mollit anim id est laborum.";
+            "deserunt mollit anim id est laborum. LL";
 
     public static void main(String[] args) {
 
@@ -15,10 +15,19 @@ public class Main {
 
         for (int i = 0; i < text.length(); i++) {
             char tmp = text.charAt(i);
-            if (Character.isLetter(tmp)){
-                textSearch.put(tmp, i);
+
+
+            if (Character.isLetter(tmp)) {
+                int key = 1;
+                textSearch.put(tmp, key);
+
+                if (textSearch.containsKey(tmp)) {
+                    int tmpkey = textSearch.get(tmp);
+                    textSearch.put(tmp, tmpkey + 1);
+                }
             }
         }
-        //System.out.println(textSearch);
+
+        System.out.println(textSearch);
     }
 }
